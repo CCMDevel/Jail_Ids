c887567041bb4f8cd0fe6820b844642026732f5f
CCMDevel <chris.j.mcgrath@gmail.com>
2016-04-29 17:41:44 -0400

    made person unique across name; forced names to be uppercase

--- app/models/person.model.js
+++ app/models/person.model.js
@@ -21,4 +21,44 @@ var PersonSchema = new Schema({
     }
 });
 
+PersonSchema.index({
+    name_first : 1,
+    name_middle : 1,
+    name_last : 1
+});
+
+var nameToUpperCase = function(obj){
+    if (obj.name_first) obj.name_first = obj.name_first.toUpperCase();
+    if (obj.name_middle) obj.name_middle = obj.name_middle.toUpperCase();
+    if (obj.name_last) obj.name_last = obj.name_last.toUpperCase();
+};
+
+var logName = function(obj){
+    temp = {};
+    if (obj.name_first) temp.name_first = obj.name_first;
+    if (obj.name_middle) temp.name_middle = obj.name_middle;
+    if (obj.name_last) temp.name_last = obj.name_last;
+    console.log('    temp = ' + JSON.stringify(temp));   
+};
+
+PersonSchema.pre('save', function(next){
+    nameToUpperCase(this);
+    next();
+});
+
+PersonSchema.pre('remove', function(next){
+    nameToUpperCase(this);
+    next();    
+});
+
+PersonSchema.pre('find', function(next){
+    nameToUpperCase(this._conditions);
+    next();    
+});
+
+PersonSchema.pre('findOne', function(next){
+    nameToUpperCase(this._conditions);
+    next();    
+});
+
 mongoose.model('Person', PersonSchema);
