382c62fc650ea54733eadad2265ea79be789571e
CCMDevel <chris.j.mcgrath@gmail.com>
2016-04-29 17:42:32 -0400

    included new routes

--- config/jail_ids.routes.js
+++ config/jail_ids.routes.js
@@ -9,6 +9,7 @@ module.exports = function(app, homeDir){
     var routesDir = '../app/routes/';
 
     require(routesDir + 'web_app.routes.js')(app, homeDir);
+    require(routesDir + 'person.routes.js')(app);
 
     console.log('    routes initialized.');
 };
